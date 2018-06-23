98cd125f1230db313c53d2bac791a1096179bb8f
dev1203 <devsingh120397@gmail.com>
2018-06-23 16:03:15 -0700

    Game Completed

--- pig-game-JS/app.js
+++ pig-game-JS/app.js
@@ -14,6 +14,7 @@ var scores,round_score,active_player,dice;
 scores = [0,0];
 round_score = 0;
 active_player = 0;
+
 document.querySelector('.dice').style.display='none';
 document.getElementById('current-0').textContent='0';
 document.getElementById('current-1').textContent='0';
@@ -25,17 +26,43 @@ document.querySelector(".btn-roll").addEventListener('click',function(){
     document.querySelector('.dice').style.display='block';
     document.querySelector('.dice').src = './dice-' + dice + '.png';
     if( dice !==1 ) {
-        var current_score=Number(document.querySelector('#current-' + active_player).textContent);
-        current_score+=dice;
-        document.querySelector('#current-' + active_player).textContent=current_score;
-    }else{
+
+        round_score=Number(document.querySelector('#current-' + active_player).textContent);
+        round_score+=dice;
+        document.querySelector('#current-' + active_player).textContent=round_score;
+
+    }
+    else{
+
+        document.querySelector('#current-' + active_player).textContent='0';
+        document.querySelector('.player-'+active_player+'-panel').classList.toggle('active');
+
+        if(active_player==0){
+            active_player=1;
+        }
+
+        else{
+            active_player=0;
+        }
+
+        document.querySelector('.player-'+active_player+'-panel').classList.add('active');
+        document.querySelector('.dice').style.display='none';
+
+    }
+
+});
+document.querySelector('.btn-hold').addEventListener('click',function(){
+        scores[active_player]+=Number(document.querySelector('#current-' + active_player).textContent);
+        document.querySelector('#score-'+ active_player).textContent=(scores[active_player]);
+        document.querySelector('#current-' + active_player).textContent='0';
+        document.querySelector('.player-'+active_player+'-panel').classList.toggle('active');
+
         if(active_player==0){
             active_player=1;
         }
         else{
             active_player=0;
         }
-
-    }
-
+        document.querySelector('.player-'+active_player+'-panel').classList.add('active');
+        document.querySelector('.dice').style.display='none';
 });
