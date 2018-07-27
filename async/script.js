de62635c4152e71e95ed8856a088fdd443cac090
Dev Singh <devsingh120397@gmail.com>
2018-07-26 21:46:31 -0700

    async/index.html

--- async/script.js
+++ async/script.js
@@ -1,26 +1,26 @@
-function one(){
-	return new Promise((resolve,reject)=>{
-		setTimeout(()=>{
-			resolve([10,20,30,40]);
-		},1500);
-	});
-}
-function two(){
-	return new Promise((resolve,reject)=>{
-		setTimeout(()=>{
-			resolve([111,222,333,444])
-		},1500);
-	});
-}
+// function one(){
+// 	return new Promise((resolve,reject)=>{
+// 		setTimeout(()=>{
+// 			resolve([10,20,30,40]);
+// 		},1500);
+// 	});
+// }
+// function two(){
+// 	return new Promise((resolve,reject)=>{
+// 		setTimeout(()=>{
+// 			resolve([111,222,333,444])
+// 		},1500);
+// 	});
+// }
 
-one().
-then((result)=>{
-	console.log(...result);
-	return two();
-})
-.then((el)=>{
-	console.log(...el);
-})
-.catch((error)=>{
-	console.log(error);
-})
+// one().
+// then((result)=>{
+// 	console.log(...result);
+// 	return two();
+// })
+// .then((el)=>{
+// 	console.log(...el);
+// })
+// .catch((error)=>{
+// 	console.log(error);
+// })
